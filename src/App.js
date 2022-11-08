99542fdef01ff0e26cd84fe9f69bfcabbd88187b
Shivam Gupta <100027945+ShivamGupta2007@users.noreply.github.com>
2022-11-08 16:55:23 +0530

    movies-app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import MoviesApp from "./Components/MoviesApp";
+
+function App() {
+  return (
+    <div className="App">
+      <MoviesApp />
+    </div>
+  );
+}
+
+export default App;
